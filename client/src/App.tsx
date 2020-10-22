6c237cbf5e4b506f6839736935523cde30afe41f
wisthn <wisthn@gmatc.matc.edu>
2020-10-22 02:39:42 -0500

    project 1 complete

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,29 +3,42 @@ import './App.css';
 import axios from 'axios';
 
 class App extends React.Component {
-state = {
-  values: []
-};
+  state = {
+    values: []
+  };
 
-componentDidMount(){
-  axios.get('http://localhost:5000/api/values')
-    .then((response) => {
-      this.setState({
-        values: response.data
+  componentDidMount(){
+    axios.get('http://localhost:5000/api/values')
+      .then((response) => {
+        this.setState({
+          values: response.data
+        })
       })
-    })
-    .catch((error) => {
-      console.error(`Error fetching data: ${error}`);
-    });
+      .catch((error) => {
+        console.error(`Error fetching data: ${error}`);
+      });
+  }
+
+    render(){
+      return (
+        <div className="App">
+          <header className="App-header">
+            Project 1
+          </header>
+          <MainBody body={this.state.values}/>
+        </div>
+      );
+    }
 }
 
+type Props = {
+  body: string[];
+};
+class MainBody extends React.Component<Props> {
   render(){
-    return (
-      <div className="App">
-        <header className="App-header">
-          Project 1
-        </header>
-        {this.state.values.map((value: any) => <div key={value}>{value}</div>)}
+    return(
+      <div className="mainbody">
+        {this.props.body.map((value: any, index) => <p className="maintext" key={value}>{index+1}. {value}</p>)}
       </div>
     );
   }
